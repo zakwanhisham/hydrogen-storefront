63874224174e12f7eba783439db6494f66064e88
zakwan hisham <hishamzakwan@gmail.com>
2023-03-13 17:49:25 +0800

    Initial commit

---
+++ app/root.jsx
@@ -0,0 +1,69 @@
+import { Seo } from '@shopify/hydrogen';
+import {
+    Links,
+    Meta,
+    Outlet,
+    Scripts,
+    ScrollRestoration,
+    useLoaderData,
+} from '@remix-run/react';
+import tailwind from './styles/tailwind-build.css';
+import favicon from '../public/favicon.svg';
+import { Layout } from './components/Layout';
+
+export const links = () => {
+    return [
+        { rel: 'stylesheet', href: tailwind },
+        {
+            rel: 'preconnect',
+            href: 'https://cdn.shopify.com',
+        },
+        {
+            rel: 'preconnect',
+            href: 'https://shop.app',
+        },
+        { rel: 'icon', type: 'image/svg+xml', href: favicon },
+    ];
+};
+
+export const meta = () => ({
+    charset: 'utf-8',
+    viewport: 'width=device-width,initial-scale=1',
+});
+
+export async function loader({ context }) {
+    const layout = await context.storefront.query(LAYOUT_QUERY);
+    return { layout };
+}
+
+export default function App() {
+    const data = useLoaderData();
+
+    const { name } = data.layout.shop;
+
+    return (
+        <html lang="en">
+            <head>
+                <Seo />
+                <Meta />
+                <Links />
+            </head>
+            <body>
+                <Layout title={name}>
+                    <Outlet />
+                </Layout>
+                <ScrollRestoration />
+                <Scripts />
+            </body>
+        </html>
+    );
+}
+
+const LAYOUT_QUERY = `#graphql
+  query layout {
+    shop {
+      name
+      description
+    }
+  }
+`;

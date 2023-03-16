b24fbd2efd2faef78c19d1f6301d286db2050adb
zakwan hisham <hishamzakwan@gmail.com>
2023-03-16 16:32:43 +0800

    Finish hydrogen

--- app/root.jsx
+++ app/root.jsx
@@ -11,6 +11,8 @@ import {
 import tailwind from './styles/tailwind-build.css';
 import favicon from '../public/favicon.svg';
 import {Layout} from './components/Layout';
+import {defer} from '@shopify/remix-oxygen';
+import {CART_QUERY} from './queries/cart';
 
 const shopifyConfig = {
   storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
@@ -41,8 +43,29 @@ export const meta = () => ({
 });
 
 export async function loader({context}) {
-  const layout = await context.storefront.query(LAYOUT_QUERY);
-  return {layout};
+  const cartId = await context.session.get('cartId');
+
+  return defer({
+    cart: cartId ? getCart(context, cartId) : undefined,
+    layout: await context.storefront.query(LAYOUT_QUERY),
+  });
+}
+
+async function getCart({storefront}, cartId) {
+  if (!storefront) {
+    throw new Error('missing storefront client in cart query');
+  }
+
+  const {cart} = await storefront.query(CART_QUERY, {
+    variables: {
+      cartId,
+      country: storefront.i18n.country,
+      language: storefront.i18n.language,
+    },
+    cache: storefront.CacheNone(),
+  });
+
+  return cart;
 }
 
 export default function App() {
--- app/routes/products/$handle.jsx
+++ app/routes/products/$handle.jsx
@@ -2,6 +2,7 @@ import {useLoaderData} from '@remix-run/react';
 import {MediaFile, Money, ShopPayButton} from '@shopify/hydrogen-react';
 import {json} from 'react-router';
 import ProductOptions from '~/components/ProductOptions';
+import {useMatches, useFetcher} from '@remix-run/react';
 
 export const loader = async ({params, context, request}) => {
   const {handle} = params;
@@ -128,7 +129,7 @@ export default function ProductHandle() {
                 variantIds={[selectedVariant?.id]}
                 width={'400px'}
               />
-              {/* TODO: product form */}
+              <ProductForm variantId={selectedVariant?.id} />
             </div>
           )}
           <div
@@ -138,6 +139,29 @@ export default function ProductHandle() {
         </div>
       </div>
     </section>
+  );
+}
+
+function ProductForm({variantId}) {
+  const [root] = useMatches();
+  const selectedLocale = root?.data?.selectedLocale;
+  const fetcher = useFetcher();
+
+  const lines = [{merchandiseId: variantId, quantity: 1}];
+
+  return (
+    <fetcher.Form action="/cart" method="post">
+      <input type="hidden" name="cartAction" value={'ADD_TO_CART'} />
+      <input
+        type="hidden"
+        name="countryCode"
+        value={selectedLocale?.country ?? 'US'}
+      />
+      <input type="hidden" name="lines" value={JSON.stringify(lines)} />
+      <button className="bg-black text-white px-6 py-3 w-full rounded-md text-center font-medium max-w-[400px]">
+        Add to Bag
+      </button>
+    </fetcher.Form>
   );
 }
 
